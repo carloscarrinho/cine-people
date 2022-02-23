8205418c2df363ab800bfbb7f13c1a7f8cf1b213
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-02-23 20:09:38 -0300

    chore: remove mongoUrlTest environment variable

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/blogv1",
-  mongoUrlTest: process.env.MONGO_URL_TEST || "mongodb://localhost:27017/blogv1_test",
+  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/people",
   port: process.env.PORT || 3333,
   rabbitMQ: process.env.RABBIT_MQ_URL || "amqp://user:1234@localhost:5672"
 };
